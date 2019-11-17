b18f0894e58aba5d53266d828fb79011dc5987b5
Bulatbik <bulatmegavolt@gmail.com>
2019-11-16 20:03:14 -0500

    Added k-fold validation and comments

--- src/numpyCreate.py
+++ src/numpyCreate.py
@@ -3,25 +3,19 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report, confusion_matrix
-from sklearn import svm
-dataclean = np.genfromtxt("../data/adult.data.cleaned2", dtype='str')  # created numpy with string data
-a = np.zeros((1001,15))
+#from sklearn.model_selection import KFold
+dataclean = np.genfromtxt("../data/adult.data.cleaned2", dtype='str')
+a = np.zeros((1001,15)) #we need 'a' to create string numpy 'A' out of it
 A = a.astype(str)
 count = 0
-# in this loop removing all nonUS born from the array
-#for i in range(len(dataclean)):
-#    if dataclean[i][13] == "United-States,":
-##        A[count]=dataclean[i]
- #       count = count+1
-
 for i in range(len(dataclean)):
         A[count]=dataclean[i]
         count = count+1
 #print(A[0:,0:])
 
-df = pd.DataFrame(data=A[1:,0:],columns=A[0, 0:])
-#print(df)
-#my_dict = {"Private,":"1","Self-emp-not-inc,":"2","Self-emp-inc,":"3", "Federal-gov,":"4","Local-gov,":"5","State-gov,":"6","Without-pay,":"7","Never-worked,":"8"}
+df = pd.DataFrame(data=A[1:,0:],columns=A[0, 0:]) #creating dataframes and setting up the columns
+
+#creating categories for each column
 labelsEdu = df['education'].astype('category').cat.categories.tolist()
 labelsWork = df['workclass'].astype('category').cat.categories.tolist()
 labelsMarry = df['marital-status'].astype('category').cat.categories.tolist()
@@ -31,10 +25,12 @@ labelsRace = df['race'].astype('category').cat.categories.tolist()
 labelsSex = df['sex'].astype('category').cat.categories.tolist()
 labelsCountry = df['native-country'].astype('category').cat.categories.tolist()
 labelsIncome = df['income'].astype('category').cat.categories.tolist()
+#convert categorical data into numerical
 replace_map_comp = {'education' : {k: v for k,v in zip(labelsEdu,list(range(1,len(labelsEdu)+1)))}, 'workclass' : {k: v for k,v in zip(labelsWork,list(range(1,len(labelsWork)+1)))}, 'marital-status' : {k: v for k,v in zip(labelsMarry,list(range(1,len(labelsMarry)+1)))},
                     'occupation' : {k: v for k,v in zip(labelsOccup,list(range(1,len(labelsOccup)+1)))}, 'relationship' : {k: v for k,v in zip(labelsFamily,list(range(1,len(labelsFamily)+1)))}, 'race' : {k: v for k,v in zip(labelsRace,list(range(1,len(labelsRace)+1)))},
                     'sex' : {k: v for k,v in zip(labelsSex,list(range(1,len(labelsSex)+1)))}, 'native-country' : {k: v for k,v in zip(labelsCountry,list(range(1,len(labelsCountry)+1)))},'income' : {k: v for k,v in zip(labelsIncome,list(range(1,len(labelsIncome)+1)))}}
 
+#making a copy of our dataframe to work on (it is a good practice)
 df_replace = df.copy()
 df_replace=df_replace.replace(replace_map_comp)
 #getting rid of commas in initially numerical values
@@ -46,30 +42,35 @@ df_replace['capital-loss'] = df_replace['capital-loss'].str.replace(',', '')
 df_replace['hours-per-week'] = df_replace['hours-per-week'].str.replace(',', '')
 df_replace.astype('int64').dtypes
 print(df_replace)
-
-
-
-# Split the data into training and test sets
-#X_train, X_test, y_train, y_test, images_train, images_test = train_test_split(digits.data, digits.target, digits.images, test_size=0.25, random_state=42)
-
 #print(df_replace.info())  good for debug!!!
 #print(df_replace.dtypes) good for debug!!!
 
 df_replace_transposed = df_replace.values.T
-# print(df_replace_transposed)
 y = df_replace_transposed[14]
 X = df_replace.values[:1000, :14]
 X = np.asarray(X).astype(int)
 y = np.asarray(y).astype(int)
 
-# print(X)
-# print(y)
-# print(X.shape)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
-print(X_test.shape)
+# Split the data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
 svclassifier = SVC(kernel='linear')
-svclassifier.fit(X_train, y_train)
+#  kfold = KFold(3, True, 1)
+#  for train, test in kfold.split(X_train):
+#  print('train: %s, test: %s' % (X_train[train], X_train[test]))
+#  svclassifier.fit(X_train[train], y_train[train])
+#  y_pred = svclassifier.predict(X_train[test])
+#  print(confusion_matrix(y_train[test], y_pred))
+#  print(classification_report(y_train[test], y_pred))
 
+#10-k fold cross-validation
+for i in range(0,10):
+    T = set(range(int(np.floor((800 * i) / 10)), int(np.floor(((800 * (i + 1)) / 10) - 1)) + 1))
+    S = set(range(0, 800)) - T
+    svclassifier.fit(X_train[list(S)], y_train[list(S)]) #SVM learning every partition
+
+
+#svclassifier = SVC(kernel='linear')
+#svclassifier.fit(X_train, y_train)
 y_pred = svclassifier.predict(X_test)
 print(confusion_matrix(y_test,y_pred))
 print(classification_report(y_test,y_pred))
